cf21882bf872c0f0e71e7a58f64041687b3bb91d
SUJINJEONG012 <peekaboo32@naver.com>
2023-08-02 22:32:22 +0900

    여러개의 Props 비구조화 할당

--- src/App.js
+++ src/App.js
@@ -3,18 +3,10 @@ import Hello from './Hello';
 import './App.css';
 
 function App() {
-  const name= 'react';
-  const style={
-    backgroundColor:'black',
-    color:'aqua',
-    fontSize:24,
-    padding:'1rem'
-  }
   return (
     <>
-      <Hello/>
-      <div style={style}>{name}</div>
-      <div className="gray-box"></div>
+    <Hello name="react" color="red" />
+    <Hello color ="pink"/>
     </>
   );
 }
