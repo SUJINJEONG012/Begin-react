9999de4fb8b9c7f270bd494033bc2a74633bee30
SUJINJEONG012 <peekaboo32@naver.com>
2023-08-04 23:24:37 +0900

    일반배열 렌더링 했을경우

--- src/App.js
+++ src/App.js
@@ -2,19 +2,20 @@ import React from "react";
 import Counter from './Counter';
 import Hello from './Hello';
 import Wrapper from './Wrapper';
+
 import './App.css';
 import InputSample from "./InputSample";
+import UserList from "./UserList";
 
 function App() {
   return (
-   
-
     <Wrapper>
     <Hello name="react" color="red" isSpecial={true} />
     <Hello color ="pink"/>
     <Counter/>
     <br></br> <br></br> <br></br>
     <InputSample/>
+    <UserList/>
     </Wrapper>
   );
 }
