57893569742badc90c765fa72475b8c51d3b5d0a
SUJINJEONG012 <peekaboo32@naver.com>
2023-08-04 22:46:29 +0900

    prevNumber으로 함수형업데이트

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
+import Counter from './Counter';
 import Hello from './Hello';
 import Wrapper from './Wrapper';
 import './App.css';
 
 function App() {
   return (
+   
+
     <Wrapper>
     <Hello name="react" color="red" isSpecial={true} />
     <Hello color ="pink"/>
+    <Counter/>
     </Wrapper>
   );
 }
